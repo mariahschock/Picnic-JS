3111a5aed157d5b29b5f01d9bb5bac788eced09e
Mariah <mariahschock@gmail.com>
2022-04-27 11:34:49 -0700

    grapes

--- app.js
+++ app.js
@@ -9,3 +9,9 @@ const cheese = document.getElementById('cheese');
 cheese.addEventListener('click', () => {
     cheese.classList.toggle('picked');
 });
+
+const grapes = document.getElementById('grapes');
+
+grapes.addEventListener('click', () => {
+    grapes.classList.toggle('picked');
+});
