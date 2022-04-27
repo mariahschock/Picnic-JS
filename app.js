250b94288191fc57fdde14fe2b7333747ff0e776
Mariah <mariahschock@gmail.com>
2022-04-27 11:33:31 -0700

    cheese

--- app.js
+++ app.js
@@ -3,3 +3,9 @@ const soda = document.getElementById('soda');
 soda.addEventListener('click', () => {
     soda.classList.toggle('picked');
 });
+
+const cheese = document.getElementById('cheese');
+
+cheese.addEventListener('click', () => {
+    cheese.classList.toggle('picked');
+});
